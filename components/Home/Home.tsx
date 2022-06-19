f2d202cbe4a44880336faaed9c8f8f89a063d0ae
gabypega1997 <gabypega1997@gmail.com>
2022-06-19 10:27:27 +0200

    create Home

--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -1,8 +1,48 @@
 import React from "react";
+import * as XLSX from 'xlsx';
+
 
 const Home: React.FC = () => {
-
-    
+        const readExcel: any = (file:any) => {
+            const promise = new Promise((resolv,reject) =>{
+            const fileReader = new FileReader();
+            fileReader.readAsArrayBuffer(file);
+            fileReader.onload = (e:any) => {
+                const bufferArray = e.target.result;
+        
+                const wb = XLSX.read(bufferArray, {type:'buffer'});
+        
+                const wsname = wb.SheetNames[0]; 
+        
+                const ws = wb.Sheets[wsname];
+        
+                const data:any = XLSX.utils.sheet_to_json(ws);
+        
+                resolv(data);
+        
+            };
+            fileReader.onerror = ((error) => {
+                reject(error);
+            });
+            });
+        
+            promise.then((data:any) => {
+            
+            console.log(data[0].Ort);
+            })
+        }
+        
+        
+        
+        
+        return (
+            <div >
+            <input type='file' onChange={(e:any)=>{
+                const file = e.target.files[0]
+                readExcel(file)
+            }}/>
+            </div>
+        )
 }
 
 
