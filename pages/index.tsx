712edf01e57fb4e910b0cbcfaf4fb7d431f20b59
gabypega1997 <gabypega1997@gmail.com>
2022-06-21 21:12:36 +0200

    change the xlsx with json

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,51 +2,13 @@ import react from 'react';
 import type { NextPage } from 'next'
 import { resolve } from 'path'
 import styles from '../styles/Home.module.css';
-import * as XLSX from 'xlsx';
 import { promises } from 'stream';
 
 const Home: NextPage = () => {
 
-
-  let objekte:any = {};
-  const readExcel: any = (file:any) => {
-    const promise = new Promise((resolv,reject) =>{
-      const fileReader = new FileReader();
-      fileReader.readAsArrayBuffer(file);
-      fileReader.onload = (e:any) => {
-        const bufferArray = e.target.result;
-
-        const wb = XLSX.read(bufferArray, {type:'buffer'});
-
-        const wsname = wb.SheetNames[0]; 
-
-        const ws = wb.Sheets[wsname];
-
-        const data:any = XLSX.utils.sheet_to_json(ws);
-
-        resolv(data);
-
-      };
-      fileReader.onerror = ((error) => {
-        reject(error);
-      });
-    });
-
-    promise.then((data:any) => {
-      objekte = data;
-      console.log(objekte[0]);
-    })
-  }
-
-
-
-
   return (
     <div className={styles.container}>
-      <input type='file' onChange={(e:any)=>{
-        const file = e.target.files[0]
-        readExcel(file)
-      }}/>
+      
     </div>
   )
 }
