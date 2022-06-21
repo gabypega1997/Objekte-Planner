ba793ab5931950306dac3406c48535cb7051fcbf
gabypega1997 <gabypega1997@gmail.com>
2022-06-21 21:31:30 +0200

    loop throw the objekts

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,12 +3,14 @@ import type { NextPage } from 'next'
 import { resolve } from 'path'
 import styles from '../styles/Home.module.css';
 import { promises } from 'stream';
-
+import {Objekte} from '../utils/localization.json' 
 const Home: NextPage = () => {
-
+  const ort = Objekte[0].Ort;
   return (
     <div className={styles.container}>
-      
+      {Objekte.map((Objekt) => {
+        return <p key={Objekt.WirtEinh}>{Objekt.Ort} {Objekt.PLZ} {Objekt.P2}</p>
+      })}
     </div>
   )
 }
