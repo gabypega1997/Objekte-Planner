32ef6f054199dc73156f230bcbf88da5b137d7e3
gabypega1997 <gabypega1997@gmail.com>
2022-06-22 18:51:37 +0200

    add new functionality

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,11 +5,13 @@ import styles from '../styles/Home.module.css';
 import { promises } from 'stream';
 import {Objekte} from '../utils/localization.json' 
 const Home: NextPage = () => {
-  const ort = Objekte[0].Ort;
+  const ort = Objekte.filter((Objekt) => {
+    return Objekt.P2 === "25" ?  Objekt : null  
+  });
   return (
     <div className={styles.container}>
-      {Objekte.map((Objekt) => {
-        return <p key={Objekt.WirtEinh}>{Objekt.Ort} {Objekt.PLZ} {Objekt.P2}</p>
+      {ort.map((Objekt) => {
+        return <p key={Objekt.WirtEinh}>{Objekt.Ort} {Objekt.PLZ}</p>
       })}
     </div>
   )
