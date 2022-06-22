48196ac2e05e081d570cc5ae38da580cae6e9ff0
gabypega1997 <gabypega1997@gmail.com>
2022-06-22 19:54:31 +0200

    add input

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,12 +4,23 @@ import { resolve } from 'path'
 import styles from '../styles/Home.module.css';
 import { promises } from 'stream';
 import {Objekte} from '../utils/localization.json' 
+
+
+
+
+
 const Home: NextPage = () => {
+
+  const week:string = '20';
   const ort = Objekte.filter((Objekt) => {
-    return Objekt.P2 === "25" ?  Objekt : null  
+    return Object.values(Objekt).includes(week) ? Objekt : null; 
   });
+
+  console.log(ort)
   return (
     <div className={styles.container}>
+      <input type='number'/>
+
       {ort.map((Objekt) => {
         return <p key={Objekt.WirtEinh}>{Objekt.Ort} {Objekt.PLZ}</p>
       })}
